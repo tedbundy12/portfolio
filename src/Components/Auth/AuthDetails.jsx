8855013831928e78cc105dab18605baab8c41da4
unnamed <sedrakyan.878@gmail.com>
2024-11-02 21:22:33 +0400

    new interface

--- src/Components/Auth/AuthDetails.jsx
+++ src/Components/Auth/AuthDetails.jsx
@@ -22,9 +22,14 @@ const AuthDetails = () => {
     <div className="auth-details mr-12 fm:mr-[50px] ffm:ml-[0px] em:mr-[30px] tm:mr-4 flex items-center">
       {user ? (
         <div className="flex items-center tm:flex-wrap">
-          <p className="text-white mr-4 ssm:hidden">{language === "en" ? `${user.email}` : `Вы вошли как ${user.email}`}</p>
-          <button onClick={handleSignOut} className="bg-red-600 tm:mt-2 text-white px-4 py-2 rounded ssm:ml-0">
-            {language === 'en' ? 'Logout' : "Выйти"}
+          <p className="text-white mr-4 ssm:hidden">
+            {language === "en" ? `${user.email}` : `Вы вошли как ${user.email}`}
+          </p>
+          <button
+            onClick={handleSignOut}
+            className="bg-red-600 tm:mt-2 text-white px-4 py-2 rounded ssm:ml-0"
+          >
+            {language === "en" ? "Logout" : "Выйти"}
           </button>
         </div>
       ) : (
---
+++ src/Components/CategoryButtons.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { LanguageContext } from "./LanguageContext";
+
+const CategoryButtons = ({
+  categories,
+  selectedCategory,
+  onCategoryChange,
+}) => {
+  const { language, toggleLanguage } = useContext(LanguageContext);
+
+  return (
+    <div className="mb-10 flex gap-5 justify-center pt-12 sm:flex-wrap">
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={`px-4 py-2 rounded-lg border-2 border-black ${
+            selectedCategory === category
+              ? "bg-blue-600 text-white"
+              : "bg-white text-black"
+          }`}
+          onClick={() => onCategoryChange(category)}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default CategoryButtons;
